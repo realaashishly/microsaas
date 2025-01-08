062eddffc9ba55cf85ad42b4735e0c89030ccc43
Aashish Singh <11aashish.singh@gmail.com>
2025-01-08 21:42:34 +0530

    added loading state

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,24 @@
+'use client';
 import { SignInWithGoogle } from '@/lib/auth';
+import { Loader } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 export default function page() {
-   
+    const { data: session, status } = useSession();
+
+    if (status === 'loading') {
+        return (
+            <div className='w-screen h-screen flex flex-col space-y-4 items-center justify-center'>
+                <span className='animate-spin'>
+                    <Loader />
+                </span>
+                <p className='text-zinc-700'>Setting up your account</p>
+            </div>
+        );
+    }
+
+    if (status === 'authenticated') redirect('/dashboard');
 
     return (
         <div>
